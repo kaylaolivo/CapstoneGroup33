3e9cca4897854a16555677dbc5fc1f36e4c10eb2
menaman123 <mena120600@gmail.com>
2024-02-09 16:09:18 -0500

    new file:   backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        required : true},
+    dob: {
+        type: Date,
+        required: true},
+});
+
+userSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+
+module.exports = mongoose.model('User', userSchema);
