a4e815f053d03e761f1b9fd07f09262c1720d62b
Yiran <yzhang683@uiowa.edu>
2024-03-19 18:16:43 -0400

    Updated Places

--- frontend/src/pages/Places.js
+++ frontend/src/pages/Places.js
@@ -1,21 +1,22 @@
-// /pages/places.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Places.css'; 
 
 // Importing Pages
 import NewPlace from '../places/pages/NewPlace';
 import UserPlaces from '../places/pages/UserPlaces';
 import UpdatePlace from '../places/pages/UpdatePlace';
 
-
-// Places component (main page)
 const Places = () => {
   return (
     <Router>
-      <div>
-
+      <div className="router-container">
+        <div className="link-container">
+          <Link to="/places/new">New Place</Link>
+          <Link to="/places/update">Update Place</Link>
+          <Link to="/places/user">User Places</Link>
+        </div>
 
         {/* Routes for sub-pages */}
         <Route path="/places/new" component={NewPlace} />
