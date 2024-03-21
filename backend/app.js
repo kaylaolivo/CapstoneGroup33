eb4e60c4657211debf2d5205c62754d20a7dd2c7
unknown <hk772@scarletmail.rutgers.edu>
2024-03-21 12:55:15 -0400

    Add books, catalog finished, search halfway

--- backend/app.js
+++ backend/app.js
@@ -136,3 +136,5 @@ app.use("/auth", authRoutes);
 app.get("/", (req, res) => res.send("Hello world!"));
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
